1cf0c3e817328ef8ce0c2ab11ca52b8f5560e37e
mima <mima@computas.com>
2022-08-05 14:13:04 +0200

    removed material. added a bunch of modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,32 @@
+// Angular Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {CommonModule} from "@angular/common";
 
+// Third Party Modules
+
+
+// Page Modules
+import {PagesModule} from "./pages-module/pages.module";
+
+// Feature Modules
+
+
+// App Modules
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import {CommonModule} from "@angular/common";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     CommonModule,
-    MatToolbarModule,
-    MatIconModule,
+    PagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages-module/pages.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPageModule } from "./main-page/main-page.module";
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MainPageModule,
+  ]
+})
+export class PagesModule { }
