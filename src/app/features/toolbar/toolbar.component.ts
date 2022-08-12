64350a870b39ca161d16b9f7633caca96f3af33c
mima <mima@computas.com>
2022-08-12 14:01:56 +0200

    toolbar styled

--- src/app/features/toolbar/toolbar.component.ts
+++ src/app/features/toolbar/toolbar.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
 })
-export class ToolbarComponent {}
+export class ToolbarComponent implements OnInit {
+  hidden: boolean;
+
+  ngOnInit(): void {
+    this.hidden = true;
+  }
+
+  setHidden() {
+    this.hidden = !this.hidden
+  }
+}
