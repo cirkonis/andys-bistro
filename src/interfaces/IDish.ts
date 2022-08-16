c3b4d497fa1e2cc076d1b7f4e57e1af3031718c3
mima <mima@computas.com>
2022-08-16 10:13:31 +0200

    added translated name to dish data

--- src/interfaces/IDish.ts
+++ src/interfaces/IDish.ts
@@ -2,9 +2,11 @@ import {IDishIngredient} from "./IDishIngredient";
 
 export interface IDish {
   name: string;
-  description?: string;
-  disclaimer?: string;
+  translatedName: string;
   edible: boolean;
   ingredients: IDishIngredient[];
   buffet?: boolean;
+  description?: string;
+  disclaimer?: string;
+  green?: boolean;
 }
--- src/store/menu-data/dish-data.ts
+++ src/store/menu-data/dish-data.ts
@@ -4,10 +4,11 @@ import * as Ingredients from './ingredient-data'
 // BREAD
 export const BREAD: IDish ={
   name: 'Pain Frais',
-  description: 'Freshly made every day!',
+  description: `It's BREAD!! Freshly made every day!`,
   edible: true,
   ingredients:[Ingredients.BREAD],
   buffet: true,
+  translatedName: 'Fresh Baked Bread',
 }
 
 
@@ -19,6 +20,7 @@ export const BREAKFAST_BUFFET: IDish = {
   edible: true,
   ingredients: [],
   buffet: true,
+  translatedName: 'Breakfast Buffet',
 }
 
 export const LUNCH_BUFFET: IDish = {
@@ -27,6 +29,7 @@ export const LUNCH_BUFFET: IDish = {
   edible: true,
   ingredients: [],
   buffet: true,
+  translatedName: 'Danish Cold Table-ish',
 }
 
 export const BRUNCH_BUFFET: IDish = {
@@ -35,6 +38,7 @@ export const BRUNCH_BUFFET: IDish = {
   edible: true,
   ingredients: [],
   buffet: true,
+  translatedName: 'Random Brunch of Stuff',
 }
 
 // SUNDAY DISHES
@@ -42,27 +46,31 @@ export const BRUNCH_BUFFET: IDish = {
 export const RATATOUILLE: IDish ={
   name: `La Célèbre Ratatouille d'Andy`,
   edible: true,
-  ingredients: [Ingredients.POTATO, Ingredients.ONION, Ingredients.GARLIC, Ingredients.GINGER, Ingredients.SQUASH, Ingredients.TOMATO, Ingredients.RED_CHILLI, Ingredients.EGGPLANT, Ingredients.BELL_PEPPER, Ingredients.BASIL]
+  ingredients: [Ingredients.POTATO, Ingredients.ONION, Ingredients.GARLIC, Ingredients.GINGER, Ingredients.SQUASH, Ingredients.TOMATO, Ingredients.RED_CHILLI, Ingredients.EGGPLANT, Ingredients.BELL_PEPPER, Ingredients.BASIL],
+  translatedName: `Andy's Famous Ratatouille`,
 }
 
-export const BRUSCHETTA: IDish ={
+export const BRUSCHETTA: IDish = {
   name: `Bruschetta Française`,
   disclaimer: 'Mozzarella-less will also be served for the lactose intolerant and vegans',
   edible: true,
-  ingredients: [Ingredients.BREAD, Ingredients.TOMATO, Ingredients.GARLIC, Ingredients.ONION, Ingredients.BASIL, Ingredients.MOZZARELLA]
+  ingredients: [Ingredients.BREAD, Ingredients.TOMATO, Ingredients.GARLIC, Ingredients.ONION, Ingredients.BASIL, Ingredients.MOZZARELLA],
+  translatedName: 'French Bruschetta',
 }
 
 export const BRUSCHETTA_BREADLESS: IDish ={
   name: `Bruschetta Française sans Pain`,
   disclaimer: 'Mozzarella-less will also be served for the lactose intolerant and vegans',
   edible: true,
-  ingredients: [Ingredients.SQUASH, Ingredients.TOMATO, Ingredients.GARLIC, Ingredients.ONION, Ingredients.BASIL, Ingredients.MOZZARELLA]
+  ingredients: [Ingredients.SQUASH, Ingredients.TOMATO, Ingredients.GARLIC, Ingredients.ONION, Ingredients.BASIL, Ingredients.MOZZARELLA],
+  translatedName: 'Breadless Bruschetta',
 }
 
 export const BROCCOLI_SALAD: IDish ={
   name: `Salade de Broccoli`,
   edible: true,
-  ingredients: [Ingredients.BROCCOLI, Ingredients.CARROT, Ingredients.ONION, Ingredients.VEGAN_RANCH, Ingredients.SUNFLOWER_SEEDS, Ingredients.DRIED_CRANBERRIES]
+  ingredients: [Ingredients.BROCCOLI, Ingredients.CARROT, Ingredients.ONION, Ingredients.VEGAN_RANCH, Ingredients.SUNFLOWER_SEEDS, Ingredients.DRIED_CRANBERRIES],
+  translatedName: 'Broccoli Salad',
 }
 
 // MONDAY DISHES
@@ -71,24 +79,28 @@ export const PASTA_SALAD: IDish ={
   name: `Salade de Pâtes`,
   edible: true,
   ingredients: [Ingredients.VEGAN_RANCH, Ingredients.PASTA, Ingredients.TOMATO, Ingredients.BROCCOLI, Ingredients.CAULIFLOWER, Ingredients.PEAS, Ingredients.CARROT, Ingredients.CUCUMBER],
+  translatedName: 'Pasta Salad',
 }
 
 export const CASSEROLE: IDish ={
-  name: 'Casserole de Haricots Verts',
+  name: 'The Monsieur Green Bean Casserole',
   edible: true,
   ingredients: [Ingredients.GREEN_BEANS, Ingredients.ONION, Ingredients.MUSHROOM, Ingredients.TURKEY_BACON, Ingredients.BUTTER, Ingredients.SHALLOTS, Ingredients.CHICKEN_BROTH, Ingredients.CREAM],
+  translatedName: 'The Mr. Green Bean Casserole',
 }
 
 export const CASSOULET: IDish ={
   name: `Cassoulet aux Champignon Fumé`,
   edible: true,
   ingredients: [Ingredients.SHALLOTS, Ingredients.GARLIC, Ingredients.CARROT, Ingredients.CELERY, Ingredients.WHITE_WINE, Ingredients.BEANS, Ingredients.VEGETABLE_BROTH, Ingredients.TOMATO, Ingredients.MUSHROOM],
+  translatedName: 'Smokey Mushroom Cassoulet',
 }
 
 export const CHICKPEA_SALAD: IDish ={
   name: `Salade de Pois Chiches`,
   edible: true,
   ingredients: [Ingredients.CHICK_PEAS, Ingredients.CUCUMBER, Ingredients.TOMATO, Ingredients.BELL_PEPPER, Ingredients.ONION, Ingredients.LEMON],
+  translatedName: 'Chick Pea Salad',
 }
 
 //TUESDAY DISHES
@@ -97,29 +109,34 @@ export const POTATO_SALAD: IDish ={
   name: `Salade de Pommes de Terre`,
   edible: true,
   ingredients: [Ingredients.VEGAN_RANCH, Ingredients.POTATO, Ingredients.TOMATO, Ingredients.BROCCOLI, Ingredients.CAULIFLOWER, Ingredients.PEAS, Ingredients.CARROT, Ingredients.CUCUMBER],
+  translatedName: 'Potato Salad',
 
 }
 
 export const BOUEF_BOURGUIGNON: IDish ={
   name: 'Bouef Bourguignon',
   edible: true,
-  ingredients: [Ingredients.BEEF, Ingredients.TURKEY_BACON, Ingredients.ONION, Ingredients.SHALLOTS, Ingredients.CARROT, Ingredients.GARLIC, Ingredients.RED_WINE, Ingredients.BUTTER]
+  ingredients: [Ingredients.BEEF, Ingredients.TURKEY_BACON, Ingredients.ONION, Ingredients.SHALLOTS, Ingredients.CARROT, Ingredients.GARLIC, Ingredients.RED_WINE, Ingredients.BUTTER],
+  translatedName: 'Beef Bourguignon',
 }
 
 export const MUSHROOM_BOURGUIGNON: IDish ={
-  name: 'Mushroom Bourguignon',
+  name: 'Champignon Bourguignon',
   edible: true,
-  ingredients: [Ingredients.ONION, Ingredients.GARLIC, Ingredients.MUSHROOM, Ingredients.RED_WINE, Ingredients.TOMATO, Ingredients.CARROT, Ingredients.CELERY, Ingredients.VEGETABLE_BROTH, Ingredients.FLOUR]
+  ingredients: [Ingredients.ONION, Ingredients.GARLIC, Ingredients.MUSHROOM, Ingredients.RED_WINE, Ingredients.TOMATO, Ingredients.CARROT, Ingredients.CELERY, Ingredients.VEGETABLE_BROTH, Ingredients.FLOUR],
+  translatedName: 'Mushroom Bourguignon'
 }
 
 export const GOAT_CHEESE_BAGUETTE: IDish ={
   name: 'Baguette au Chèvre',
   edible: true,
-  ingredients: [Ingredients. GOAT_CHEESE, Ingredients.BREAD,Ingredients.MAPLE_SYRUP]
+  ingredients: [Ingredients. GOAT_CHEESE, Ingredients.BREAD,Ingredients.MAPLE_SYRUP],
+  translatedName: 'Goat Cheese Baguette',
 }
 
 export const LENTIL_SALAD: IDish ={
   name: 'Salade de Lentilles',
   edible: true,
-  ingredients: [Ingredients.LENTILS, Ingredients.AVOCADO, Ingredients.SPINNACH, Ingredients.WALNUTS, Ingredients.APPLE, Ingredients.POMEGRANATE, Ingredients.ORANGE, Ingredients.TAHINI, Ingredients.GARLIC]
+  ingredients: [Ingredients.LENTILS, Ingredients.AVOCADO, Ingredients.SPINNACH, Ingredients.WALNUTS, Ingredients.APPLE, Ingredients.POMEGRANATE, Ingredients.ORANGE, Ingredients.TAHINI, Ingredients.GARLIC],
+  translatedName: 'Lentil Salad',
 }
